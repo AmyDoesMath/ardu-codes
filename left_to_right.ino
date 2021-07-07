e2bca5075e7c8b7cd1e42a5b95483aab53579088
SemicolumnDev <73789337+SemicolumnDev@users.noreply.github.com>
2021-07-07 12:43:48 +0300

    left to right

---
+++ left_to_right.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+
+void loop() {
+    for(int i = 2; i<=4; i++) {
+       digitalWrite(i, HIGH);
+       delay(100);
+       digitalWrite(i, LOW);
+       delay(100);
+    }
+    for(int i = 4; i>=2; i--) {
+       digitalWrite(i, HIGH);
+       delay(100);
+       digitalWrite(i, LOW);
+       delay(100);
+    }
+}
