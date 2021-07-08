f37e6aebc25247caa3d345d61b83871a2d7c8945
SemicolumnDev <73789337+SemicolumnDev@users.noreply.github.com>
2021-07-08 12:20:50 +0300

    "documentation"

--- joystick.ino
+++ joystick.ino
@@ -1,4 +1,12 @@
-int x=0;
+// CONNECTIONS
+// A0: Joystick X
+// A1: Joystick Y
+// 2: Joystick Button
+// 8: LED X
+// 9: LED Y
+// 10: LED Button
+
+x=0;
 int y=0;
 int b=0;
 void setup() {
@@ -7,11 +15,13 @@ void setup() {
 }
 void loop()
 {
-  x=analogRead(A0);
-  y=analogRead(A1);
-  b=digitalRead(2);
+  x=analogRead(A0); // x coordinate of joystick
+  y=analogRead(A1); // y coordinate of joystick
+  b=digitalRead(2); // button pressed indicator
+  
+  // If x coordinate > than y coordinate, it turns 8 to HIGH and vice versa.
   if(x > y) {
-    digitalWrite(8, HIGH);  
+    digitalWrite(8, HIGH);
     digitalWrite(9, LOW);  
   } else if if(x == y) {
     digitalWrite(8, HIGH);  
@@ -20,7 +30,7 @@ void loop()
     digitalWrite(8, LOW);  
     digitalWrite(9, HIGH);  
   }
-
+  // If button is pressed, turn 10 to HIGH
   if (b == 0) {
       digitalWrite(10, HIGH);
   } else {
